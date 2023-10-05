029db85b1074e279390827fb602ef220ee9c5ca4
rajat397 <rajat242003@gmail.com>
2023-10-05 09:08:07 +0530

    initial commit

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,39 +1,40 @@
 /* eslint-disable */
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { api } from "../api";
 import BlogCard from "../components/BlogCard";
 
-export default function Dashboard ({user}){
-    const [blogs,setBlogs] = useState([]);
+export default function Dashboard({ user }) {
+  const [blogs, setBlogs] = useState([]);
 
-    useEffect(() => {
-        let isSubscribed = true;
-        const fetchData = async () => {
-          const data = await api.getAllblogs();
-          
-          if (isSubscribed) {
-            setBlogs(data.data);
-          }
-          
-        };
-        fetchData().catch(console.error);
-        
-        
-        return () => (isSubscribed = false);
-      }, []);
-    
-    return (
-        <div className="mt-32 row ">
-          <div className="grid grid-cols-2 gap-4 content-center">
-             {!!blogs.length && blogs.map((blog, _index)=>(
-                      <div
-                      key={_index}
-                      className="snap-start lg:snap-center lg:min-w-[50vw] h-[100vh] flex justify-center items-center"
-                    >          
-                <BlogCard  blog = {blog}/> 
-                </div>
-            ))}
-          </div>
-        </div>
-    );
+  useEffect(() => {
+    let isSubscribed = true;
+    const fetchData = async () => {
+      const data = await api.getAllblogs();
+
+      if (isSubscribed) {
+        setBlogs(data.data);
+      }
+    };
+    fetchData().catch(console.error);
+
+    return () => (isSubscribed = false);
+  }, []);
+
+  return (
+    <div className="mt-32">
+      <div className="grid grid-cols-2 gap-x-1 gap-y-20">
+        {" "}
+        {/* Added gap-x-2 to reduce horizontal gap */}
+        {!!blogs.length &&
+          blogs.map((blog, _index) => (
+            <div
+              key={_index}
+              className="w-full flex justify-center items-center"
+            >
+              <BlogCard blog={blog} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
